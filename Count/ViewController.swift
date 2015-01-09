66559e2f8cfc011dac6aa15dab866b6837a01aa4
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-01-09 18:44:07 +0900

    swift count app

---
+++ Count/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by nagata on 1/9/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var number : Int = 0
+    @IBOutlet var label : UILabel! = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func puls(){
+        number = number + 1
+        label.text = "\(number)"
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
